8697d68a26fd4f2611883ea89974816b50b041f5
JordanB <65307981+ubern00bie@users.noreply.github.com>
2020-07-19 14:19:45 -0500

    updated functionality of search and city buttons

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-var cities = ["Kansas City", "San Diego", "Honolulu", "St Louis"];
+weatherApp();
+function weatherApp(){
+// var cities = ["Kansas City", "San Diego", "Honolulu", "St. Louis"];
+
+var cities = (JSON.parse(localStorage.getItem("weather")) || ["Kansas City", "San Diego", "Honolulu", "St. Louis"])
+
 var APIKey = "83df096b794f8f9c6911f7ddaae5235f";
-// var userCity = $('#userCity').val()
-var userCity = "Kansas City" //for testing
-// $('#userCity').val("enter city")
-$('#userCity').attr('placeholder', 'Enter city name')
-//console.log($('#userCity').val())
-$('#day3').find('h5.date').html("something");
+var userCity = $('#userCity').val();
+if (userCity === "") {
+    var userCity = 'Kansas City'
+    $('#userCity').attr('placeholder', 'City,State')
+    var queryCurrent = "https://api.openweathermap.org/data/2.5/weather?q="+ userCity +"&appid=" + APIKey;
+    var query5day = "https://api.openweathermap.org/data/2.5/forecast?q="+ userCity +"&appid=" + APIKey;
+}
+else {
 var queryCurrent = "https://api.openweathermap.org/data/2.5/weather?q="+ userCity +"&appid=" + APIKey;
 var query5day = "https://api.openweathermap.org/data/2.5/forecast?q="+ userCity +"&appid=" + APIKey;
-currentDay = moment().format('M/D/YYYY');
-// We then created an AJAX call
-getWeather();
+}
 
-// $('.cityBtn').click( function() {
-//     userCity = this.innerHTML
-//     console.log(this)
-//     console.log(userCity)
-//     getWeather();
-// })
+currentDay = moment().format('M/D/YY');
 
-function getWeather() {
 $.get(queryCurrent).then(function(response) {
+    $('.cityList').empty();
+  
     for (var i = 0; i < cities.length; i++) {
         var $cityButton = $('<button>');
         $('.cityList').append($cityButton);
@@ -28,6 +29,12 @@ $.get(queryCurrent).then(function(response) {
         $cityButton.text(cities[i])
     }
 
+    $('.cityBtn').click( function() {
+        $('#userCity').val($(this).html())
+        console.log($(this).html())
+        weatherApp()
+    })
+    
   var lat = (response.coord.lat);
   var lon = (response.coord.lon);
 
@@ -50,12 +57,9 @@ $.get(queryCurrent).then(function(response) {
             $('.UV').css('color','white');
         }
         else {
-            console.log('low')
             $('.UV').css('');
         }
     })
-
-  console.log("queryCurrent" + response);
   $('.city').html('<h1>' + response.name +",  ("+ currentDay +")"+'</h1>');
   $('.coord').html("(Latitude: " + lat + "  Longitude: " + lon +")");
   $('.wind').html("Wind Speed: "+ response.wind.speed + ' mph');
@@ -66,32 +70,34 @@ $.get(queryCurrent).then(function(response) {
 })
 
 $.get(query5day).then(function(response) {
-  console.log("query5day Call",response);
     //set 5 day forcast
  var count = 0;
     for(var i = 0; i < 5; i++) {        
         $('.date'+ i).html(response.list[count].dt_txt)
-    var icon = response.list[count].weather[0].main
-    //if else to determine icon depicted
-
         $('.img'+ i).attr('src', "images/" + response.list[count].weather[0].icon + ".png")
         var tempF = ((response.list[count].main.temp -273.15)* 1.80 + 32).toFixed(2);
-        $('.temp'+ i).html("Temp: " + tempF + "&#176F")//needs temp conversion
+        $('.temp'+ i).html("Temp: " + tempF + "&#176F")
         $('.hum'+ i).html("Humidity: " + response.list[count].main.humidity +"%")
         count = count + 8;
     }
 })
-}
 
 $("#searchBtn").on("click", function (event) {
     event.preventDefault();
-    var $userCity = $("#city-input").val();
-    cities.push($userCity);
-    localStorage.setItem("weather", JSON.stringify(cities))
-    //don't allow undefined pushes
-    console.log(cities)
+    userCity = $('#userCity').val()
+    weatherApp();
+    if (userCity === "") {
+        alert("Please enter a city name")
+    }
+    else{
+    var userCity = $('#userCity').val();
+    cities.push(userCity);
+    let citiesFilt = [...new Set(cities)];
+    localStorage.setItem("weather", JSON.stringify(citiesFilt))
+    console.log("cities:" + citiesFilt)
+    }
 });
-
+}
   // Create CODE HERE to Log the queryURL
 //   var newvar = $.get(queryCurrent);
  //weather dashboard
