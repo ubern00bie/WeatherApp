791beb4990dcc310e987e8c111852c559c92d1be
JordanB <65307981+ubern00bie@users.noreply.github.com>
2020-07-21 13:46:10 -0500

    updates to CSS, README, and comments

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 weatherApp();
-function weatherApp(){
 
+function weatherApp(){
+//gets "weather" array for search buttons from localstorage OR creates a default array if one does not exist, declares variables used in API call.
 var cities = (JSON.parse(localStorage.getItem("weather")) || ["Kansas City", "San Diego", "Honolulu", "St. Louis"])
 var APIKey = "83df096b794f8f9c6911f7ddaae5235f";
 var userCity = $('#userCity').val();
 
+//if the search box is place, adds a placeholder and runs an API call for 'Kansas City' by default
 if (userCity === "") {
     var userCity = 'Kansas City'
     $('#userCity').attr('placeholder', 'Enter City Name')
     var queryCurrent = "https://api.openweathermap.org/data/2.5/weather?q="+ userCity +"&appid=" + APIKey;
     var query5day = "https://api.openweathermap.org/data/2.5/forecast?q="+ userCity +"&appid=" + APIKey;
-}
+} 
+// if city is entered, runs API call for that city name
 else {
 var queryCurrent = "https://api.openweathermap.org/data/2.5/weather?q="+ userCity +"&appid=" + APIKey;
 var query5day = "https://api.openweathermap.org/data/2.5/forecast?q="+ userCity +"&appid=" + APIKey;
 }
 
 currentDay = moment().format('M/D/YY');
-
+//Queries weather data for the current day and generates buttons for each city that has been searched
 $.get(queryCurrent).then(function(response) {
     $('.cityList').empty();
   
@@ -33,32 +36,36 @@ $.get(queryCurrent).then(function(response) {
         console.log($(this).html())
         weatherApp()
     })
-    
-  var lat = (response.coord.lat);
-  var lon = (response.coord.lon);
+    // grabs latitude and longitude from query response and plugs them into the 5 day weather forcast query
+    var lat = (response.coord.lat);
+    var lon = (response.coord.lon);
 
-  var queryUV = "http://api.openweathermap.org/data/2.5/uvi?appid="+ APIKey +"&lat="+ lat+"&lon=" + lon;
-    $.get(queryUV).then(function(response) {
+    var queryUV = "http://api.openweathermap.org/data/2.5/uvi?appid="+ APIKey +"&lat="+ lat+"&lon=" + lon;
+        $.get(queryUV).then(function(response) {
 
-        $('.UV').html("UV Index: " + response.value)
+            $('.UV').html("UV Index: " + response.value)
 
-        if(response.value >3 && response.value < 6) {
-            $('.UV').css('background-color','yellow');
-        }
-        else if (response.value >5 && response.value < 8) {
-            $('.UV').css('background-color','orange');
-        }
-        else if (response.value >7 && response.value < 11){
-            $('.UV').css('background-color','coral');
-        }
-        else if (response.value > 10) {
-            $('.UV').css('background-color','red');
-            $('.UV').css('color','white');
-        }
-        else {
-            $('.UV').css('');
-        }
-    })
+            if(response.value >3 && response.value < 6) {
+                $('.UV').css('color','black');
+                $('.UV').css('background-color','yellow');
+            }
+            else if (response.value >5 && response.value < 8) {
+                $('.UV').css('color','black');
+                $('.UV').css('background-color','orange');
+            }
+            else if (response.value >7 && response.value < 11){
+                $('.UV').css('color','black');
+                $('.UV').css('background-color','coral');
+            }
+            else if (response.value > 10) {
+                $('.UV').css('background-color','red');
+                $('.UV').css('color','white');
+            }
+            else {
+                $('.UV').css('color','black');
+                $('.UV').css('background-color','none');
+            }
+        })
   $('.city').html('<h1>' + response.name +",  ("+ currentDay +")"+'</h1>' + '<img src=images/' + response.weather[0].icon +'.png>');
   $('.coord').html("(Latitude: " + lat + "  Longitude: " + lon +")");
   $('.wind').html("Wind Speed: "+ response.wind.speed + ' mph');
